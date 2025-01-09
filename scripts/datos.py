9095527c525a76847f745f088f08c223b9c116b8
Ralo <gpfernandezflorio@gmail.com>
2025-01-08 21:25:44 -0300

    Avanzo con ts2js

--- scripts/datos.py
+++ scripts/datos.py
@@ -1,12 +1,20 @@
 class Archivo(object):
-  def __init__(self, nombre, ruta):
+  def __init__(self, nombre, ruta, ast):
     self.nombre = nombre
     self.ruta = ruta
-  def Mostrar(self, i):
+    self.ast = ast
+  def limpiarRuta(self, pre):
+    self.ruta = self.ruta[pre:]
+  def contenido(self):
+    return self.ast.toJs()
+  def Mostrar(self, i, mostrarAST=False):
     tab = ""
     for j in range(i):
       tab += "  "
     print(f"{tab}{self.nombre} ({self.ruta})")
+    if mostrarAST:
+      print(self.ast)
+      print("\n\n")
 
 class Carpeta(object):
   def __init__(self, nombre, ruta):
@@ -14,12 +22,31 @@ class Carpeta(object):
     self.ruta = ruta
     self.carpetas = []
     self.archivos = []
+  def listaDeCarpetas(self):
+    return self.carpetas
+  def listaDeArchivos(self):
+    return self.archivos
   def agregarCarpeta(self, nombreCarpeta, rutaCarpeta):
     nuevaCarpeta = Carpeta(nombreCarpeta, rutaCarpeta)
     self.carpetas.append(nuevaCarpeta)
     return nuevaCarpeta
-  def agregarArchivo(self, nombreArchivo, rutaArchivo):
-    self.archivos.append(Archivo(nombreArchivo, rutaArchivo))
+  def agregarArchivo(self, nombreArchivo, rutaArchivo, astArchivo):
+    archivo = Archivo(nombreArchivo, rutaArchivo, astArchivo)
+    self.archivos.append(archivo)
+    return archivo
+  def limpiarRuta(self, pre=None):
+    if pre is None:
+      pre = 0
+      while self.ruta.startswith("../"):
+        self.ruta = self.ruta[3:]
+        pre = pre + 3
+    else:
+      self.ruta = self.ruta[pre:]
+    if pre > 0:
+      for carpeta in self.carpetas:
+        carpeta.limpiarRuta(pre)
+      for archivo in self.archivos:
+        archivo.limpiarRuta(pre)
   def MostrarArchivos(self, i):
     tab = ""
     for j in range(i):
@@ -34,8 +61,16 @@ class Carpeta(object):
 class Datos(object):
   def __init__(self):
     self.raiz = None
+  def listaDeCarpetas(self):
+    return [self.raiz]
+  def listaDeArchivos(self):
+    return []
   def agregarCarpeta(self, nombreCarpeta, rutaCarpeta):
     self.raiz = Carpeta(nombreCarpeta, rutaCarpeta)
     return self.raiz
   def MostrarArchivos(self):
     self.raiz.MostrarArchivos(0)
+  def limpiarRutas(self):
+    self.raiz.limpiarRuta()
+  def resolverDependencias(self):
+    print("TODO")
--- scripts/ts2js.py
+++ scripts/ts2js.py
@@ -1,15 +1,15 @@
 import sys, os
 from datos import Datos
-from bibpy.archivos import nombreDe_, listaDeCarpetasEn_, listaDeArchivosEn_, existeCarpeta_Acá, nuevaRuta_
+from bibpy.archivos import *
 sys.path.insert(0, os.path.abspath(os.path.join('.','ts-parser','src')))
 from main import parsearArchivo
 
 CARPETA_SALIDA = 'output'
 datosConversion = Datos()
+generarSalida = False # True
 
 def main():
-  ConvertirCarpeta_("../../blockly/core/")
-  ConvertirArchivo_("../../blockly/core/blockly.ts")
+  ConvertirCarpeta_("../../blockly/core/clipboard")
   exit(0)
 
   if len(sys.argv) == 1:
@@ -23,12 +23,18 @@ def main():
     Boom("No se encuentra el archivo o carpeta " + ruta)
 
 def ConvertirArchivo_(rutaArchivo):
-  parsearArchivo(rutaArchivo)
+  ast = parsearArchivo(rutaArchivo, False)
+  return ast
 
 def ConvertirCarpeta_(rutaCarpeta):
-  # ReiniciarSalida()
+  if generarSalida:
+    ReiniciarSalida()
   ConvertirCarpeta_En_(rutaCarpeta, datosConversion)
-  # datosConversion.MostrarArchivos()
+  datosConversion.limpiarRutas()
+  datosConversion.MostrarArchivos()
+  datosConversion.resolverDependencias()
+  if generarSalida:
+    escribirCarpeta_En_(datosConversion, CARPETA_SALIDA)
 
 def ReiniciarSalida():
   if existeCarpeta_Acá(CARPETA_SALIDA):
@@ -41,8 +47,17 @@ def ConvertirCarpeta_En_(rutaCarpeta, destino):
   for carpeta in listaDeCarpetasEn_(rutaCarpeta):
     ConvertirCarpeta_En_(nuevaRuta_(rutaCarpeta, carpeta), nuevoDestino)
   for archivo in listaDeArchivosEn_(rutaCarpeta):
-    nuevoDestino.agregarArchivo(archivo, nuevaRuta_(rutaCarpeta, archivo))
-    parsearArchivo(nuevaRuta_(rutaCarpeta, archivo))
+    nuevaRuta = nuevaRuta_(rutaCarpeta, archivo)
+    ast = ConvertirArchivo_(nuevaRuta)
+    nuevoDestino.agregarArchivo(archivo, nuevaRuta, ast)
+
+def escribirCarpeta_En_(carpetaRaiz, rutaDestino):
+  for archivo in carpetaRaiz.listaDeArchivos():
+    CrearArchivo_En_Con_(archivo.nombre, rutaDestino, archivo.contenido())
+  for carpeta in carpetaRaiz.listaDeCarpetas():
+    CrearCarpeta_En_(carpeta.nombre, rutaDestino)
+    rutaCarpeta = nuevaRuta_(rutaDestino, carpeta.nombre)
+    escribirCarpeta_En_(carpeta, rutaCarpeta)
 
 if __name__ == '__main__':
   main()
